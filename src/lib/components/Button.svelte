818632fc0ff33386bafd640626ec2d5101aa1b40
Zachiah Sawyer <zachiahsawyer1@gmail.com>
2022-04-08 21:22:57 -0700

    remove svelte:element

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -7,27 +7,36 @@
 	export let wrapperClass: string = '';
 	export let icon: any | undefined = undefined;
 	export let href: string | undefined = undefined;
+
+	$: classes = `${
+		color === 'primary'
+			? 'bg-purple-600 hover:bg-purple-700 text-white'
+			: 'bg-white hover:bg-gray-100 text-gray-800'
+	} font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200 block w-full ${
+		icon ? 'px-2' : 'px-4'
+	}`;
 </script>
 
 <div class={wrapperClass}>
-	<svelte:element
-	    this={href? "a" : "button"}
-		href={href}
-		on:click
-		{type}
-		{disabled}
-		class="{color === 'primary'
-			? 'bg-purple-600 hover:bg-purple-700 text-white'
-			: 'bg-white hover:bg-gray-100 text-gray-800'} font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200 block w-full {icon
-			? 'px-2'
-			: 'px-4'}"
-	>
-		{#if icon}
-			<div class="w-8 h-8">
-				<svelte:component this={icon} />
-			</div>
-		{:else}
-			<slot />
-		{/if}
-		</svelte:element>
+	{#if href}
+		<a on:click {type} {disabled} class={classes} {href}>
+			{#if icon}
+				<div class="w-8 h-8">
+					<svelte:component this={icon} />
+				</div>
+			{:else}
+				<slot />
+			{/if}
+		</a>
+	{:else}
+		<button on:click {type} {disabled} class={classes}>
+			{#if icon}
+				<div class="w-8 h-8">
+					<svelte:component this={icon} />
+				</div>
+			{:else}
+				<slot />
+			{/if}
+		</button>
+	{/if}
 </div>
