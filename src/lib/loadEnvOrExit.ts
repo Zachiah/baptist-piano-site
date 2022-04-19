c126fb4ed24e29db867e046cfbe5b3d50f91e324
Zachiah Sawyer <zachiahsawyer1@gmail.com>
2022-04-19 10:27:23 -0700

    enable exiting

--- src/lib/loadEnvOrExit.ts
+++ src/lib/loadEnvOrExit.ts
@@ -2,10 +2,10 @@ const loadEnvOrExit = (env: Record<string, string | boolean>, key: string): stri
 	const value = env[key];
 	if (value === null || value === undefined) {
 		console.error(`Missing ${key} in .env file`);
-		// if (typeof process !== 'undefined' && process.exit) {
-		// 	process.exit(1);
-		// }
-		// throw new Error(`Missing ${key} in .env file`);
+		if (typeof process !== 'undefined' && process.exit) {
+			process.exit(1);
+		}
+		throw new Error(`Missing ${key} in .env file`);
 	}
 	return value;
 };
